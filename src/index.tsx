4ebb37b7f7e0e915d73123f1f8153fa38c790964
Jamal Gardiner <jgardiner@neat.com>
2021-11-22 19:01:17 -0500

    feat: Implement theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#3212121',
+    },
+    primary: {
+      main: '#333337',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+})
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
