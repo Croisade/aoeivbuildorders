d29531e4ec7660ed3ea31f730304bc173563a63d
Jamal Gardiner <jgardiner@neat.com>
2021-11-22 19:00:50 -0500

    feat: initial commit of appbar

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/function-component-definition */
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 0.1 }}>
+            AoEIVBuildOrders.gg
+          </Typography>
+          <Button color="inherit">Github</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
